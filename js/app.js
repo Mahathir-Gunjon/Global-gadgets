963ee3aa9a44504758806d20380ec5161abafb02
Mahathir-Gunjon <mahathirgunjon@gmail.com>
2022-03-02 01:28:19 +0600

    date and optional chaining done

--- js/app.js
+++ js/app.js
@@ -1,8 +1,14 @@
+// spinner function 
 const spinner = displayStyle => {
     document.getElementById('spiner').style.display = displayStyle
 }
+// loding phone data
 const searchContentHide = displayStyle => {
     document.getElementById('card-show').style.display = displayStyle
+}
+// error text show 
+const errorText = displayStyle => {
+    document.getElementById('error').style.display = displayStyle
 }
 // input feild uel with async function
 const allPhones = async () => {
@@ -11,43 +17,40 @@ const allPhones = async () => {
     spinner('block')
     searchContentHide('none')
     searchText.value = ''
-    if(searchText.length == 0){
-        alert('search input empty')
-    }
-    else{
+    // get data from api
         const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
         const res = await fetch(url)
         const data = await res.json()
         displayReasult(data.data)
-    }
 }
 
 const displayReasult = phones => {
     // console.log(phones);
     const searchReasult = document.getElementById('search-result')
     searchReasult.textContent = '';
-    if(phones.length == 0){
-        alert('search input empty')
-    }
-    phones.forEach(phone => {
+    phones?.forEach(phone => {
         const div = document.createElement('div')
         div.classList.add('col-lg-4', 'col-md-6', 'col-12')
         div.innerHTML = `
         <div class="card border-0 shadow-lg rounded-3 pt-3 m-4 bg-white">
-        <img src="${phone.image}" class="card-img-top w-75 mx-auto pb-2" alt="...">
+            <div class="mx-auto w-75">
+                <img src="${phone.image? phone.image:'No image found'}" class="card-img-top img-fluid pb-2" alt="...">
+            </div>
             <div class="card-body">
-                <h4 class="card-title">${phone.phone_name}</h4>
+                <h4 class="card-title">${phone.phone_name? phone.phone_name:'No name found'}</h4>
                 <p class="card-text">
-                <b>Brand: </b>${phone.brand}
+                <b>Brand: </b>${phone.brand? phone.brand:'No brand found'}
                 </p>
                 <button onclick="loadPhoneDetails('${phone.slug}')" class="btn btn-style shadow fs-4 fw-normal">Explore</button>
             </div>
         </div>
         `
         searchReasult.appendChild(div)
-    });
+    });  
     spinner('none')
     searchContentHide('block')
+    errorText('none')
+    
 }
 
 // phone data load 
@@ -60,7 +63,6 @@ const loadPhoneDetails = async phoneId =>{
 }
 // full details of phone fubction 
 const detailsResult = PhoneDetails => {
-    
     const fullDetails = document.getElementById('full-detail')
     fullDetails.textContent = '';
         const div = document.createElement('div')
@@ -69,12 +71,12 @@ const detailsResult = PhoneDetails => {
         <div class="card mb-3 border-0 shadow-lg text-dark">
             <div class="row g-0">
             <div class="col-md-4">
-                <img src="${PhoneDetails.image}" class="w-100 p-4 rounded-start" alt="...">
+                <img src="${PhoneDetails.image? PhoneDetails.image:'No image found'}" class="w-100 p-4 rounded-start" alt="...">
             </div>
                 <div class="col-md-7">
                     <div class="card-body">
-                        <h1 class="card-title">${PhoneDetails.name}</h1>
-                        <p class="card-text fs-8">${PhoneDetails.releaseDate}</p>
+                        <h1 class="card-title">${PhoneDetails.name? PhoneDetails.name:'No name found'}</h1>
+                        <p class="card-text fs-8">${PhoneDetails.releaseDate? PhoneDetails.releaseDate:'No release date found'}</p>
                         <b class="fs-5">Main Features</b>
                         <table class="table">
                             <thead>
@@ -86,19 +88,19 @@ const detailsResult = PhoneDetails => {
                             <tbody>
                             <tr>
                                 <th scope="row"><b>Storage: </b></th>
-                                <td>${PhoneDetails.mainFeatures.storage}</td>
+                                <td>${PhoneDetails.mainFeatures?.storage? PhoneDetails.mainFeatures.storage:'No storage found'}</td>
                             </tr>
                             <tr>
                                 <th scope="row"><b>Display-size: </b></th>
-                                <td>${PhoneDetails.mainFeatures.displaySize}</td>
+                                <td>${PhoneDetails.mainFeatures?.displaySize? PhoneDetails.mainFeatures.displaySize:'No displaySize found'}</td>
                             </tr>
                             <tr>
                                 <th scope="row"><b>Chipset: </b></th>
-                                <td>${PhoneDetails.mainFeatures.chipSet}</td>
+                                <td>${PhoneDetails.mainFeatures?.chipSet? PhoneDetails.mainFeatures.chipSet:'No chipSet found'}</td>
                             </tr>
                             <tr>
                                 <th scope="row"><b>Memory: </b></th>
-                                <td>${PhoneDetails.mainFeatures.memory}</td>
+                                <td>${PhoneDetails.mainFeatures?.memory? PhoneDetails.mainFeatures.memory:'No memory found'}</td>
                             </tr>
                             </tbody>
                         </table>
@@ -107,48 +109,47 @@ const detailsResult = PhoneDetails => {
                             <table class="table">
                                 <tbody>
                                 <tr>
-                                    <td>${PhoneDetails.mainFeatures.sensors[0]}</td>
-                                    <td>${PhoneDetails.mainFeatures.sensors[1]}</td>
+                                    <td>${PhoneDetails.mainFeatures?.sensors[0]? PhoneDetails.mainFeatures.sensors[0]:'No sensor found'}</td>
+                                    <td>${PhoneDetails.mainFeatures?.sensors[1]? PhoneDetails.mainFeatures.sensors[1]:'No sensor found'}</td>
                                 </tr>   
-                                    <td>${PhoneDetails.mainFeatures.sensors[2]}</td>
-                                    <td>${PhoneDetails.mainFeatures.sensors[3]}</td>
+                                    <td>${PhoneDetails.mainFeatures?.sensors[2]? PhoneDetails.mainFeatures.sensors[2]:'No sensor found'}</td>
+                                    <td>${PhoneDetails.mainFeatures?.sensors[3]? PhoneDetails.mainFeatures.sensors[3]:'No sensor found'}</td>
                                 <tr>
                                 </tr>
                                 <tr>
-                                    <td>${PhoneDetails.mainFeatures.sensors[4]}</td>
-                                    <td>${PhoneDetails.mainFeatures.sensors[5]}</td>
+                                    <td>${PhoneDetails.mainFeatures?.sensors[4]? PhoneDetails.mainFeatures.sensors[4]:'No sensor found'}</td>
+                                    <td>${PhoneDetails.mainFeatures?.sensors[5]? PhoneDetails.mainFeatures.sensors[4]:'No sensor found'}</td>
                                 </tr>
                                 </tbody>
                             </table>
                         </p> 
-
                         <p class="card-text fs-6">
                             <b class="fs-5">Other details</b>
                             <table class="table">
                             <tbody>
                             <tr>
                                 <th scope="row"><b>WLAN: </b></th>
-                                <td>${PhoneDetails.others.WLAN}</td>
+                                <td>${PhoneDetails.others?.WLAN? PhoneDetails.others.WLAN:'No WLAN details found'}</td>
                             </tr>
                             <tr>
                                 <th scope="row"><b>Bluetooth: </b></th>
-                                <td>${PhoneDetails.others.Bluetooth}</td>
+                                <td>${PhoneDetails.others?.Bluetooth? PhoneDetails.others.Bluetooth:'No Bluetooth details found'}</td>
                             </tr>
                             <tr>
                                 <th scope="row"><b>GPS: </b></th>
-                                <td>${PhoneDetails.others.GPS}</td>
+                                <td>${PhoneDetails.others?.GPS? PhoneDetails.others.GPS:'No GPS details found'}</td>
                             </tr>
                             <tr>
                                 <th scope="row"><b>NFC: </b></th>
-                                <td>${PhoneDetails.others.NFC}</td>
+                                <td>${PhoneDetails.others?.NFC? PhoneDetails.others.NFC:'No NFC details found'}</td>
                             </tr>
                             <tr>
                                 <th scope="row"><b>Radio: </b></th>
-                                <td>${PhoneDetails.others.Radio}</td>
+                                <td>${PhoneDetails.others?.Radio? PhoneDetails.others.Radio:'No Radio details found'}</td>
                             </tr>
                             <tr>
                                 <th scope="row"><b>USB: </b></th>
-                                <td>${PhoneDetails.others.USB}</td>
+                                <td>${PhoneDetails.others?.USB? PhoneDetails.others.USB:'No USB details found'}</td>
                             </tr>
                             </tbody>
                         </table>
